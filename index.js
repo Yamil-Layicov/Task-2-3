80e82c768c376d9b0e56993068a568bf7ecaa262
Yamil-Layicov <yamilbl@code.edu.az>
2022-04-06 00:22:42 +0400

    Task-2-3

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+// function color() {
+//     let text = "Press a button!\nEither Ok or Cancel.";
+//     if (confirm(text) == true) {
+//       document.body.style.backgroundColor = 'black';
+//     } else {
+//         document.body.style.backgroundColor = 'white';
+//     }
+// }
+
+
+// let age = prompt('yasinizi daxil edin');
+
+// if(age<18){
+//     alert('yasiniz 18den asagidir');
+// }
+
+
+
+// console.log(age);
+
+// let drinks = prompt('what\'s your favorite drink');
+
+// switch(drinks){
+//     case 'coco-cola':
+//         console.log('i like cola');
+//     break;
+//     case 'Pepsi':
+//         console.log('i like pepsi');
+//     break;
+//     case 'seven7':
+//         console.log('i like seven7');
+//     break;
+//     default:
+//         console.log('I have never heard of that one');
+
+// }
+
+
+
+//niye confirimdeki tetxt silende eroro verir
+// function color(){
+//     document.body.style.backgroundColor = 'red';
+//on eleyende basqa reng off eleyende basqa reng olmalidi
+//switch casede nece lowercase ederdik propt misalinda pepsi
+
+
